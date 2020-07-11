eb8ad82fbc2ff100170612d298e73868296a999e
masTech Developer <ashir342@gmail.com>
2020-07-12 02:56:36 +0500

    Create Navbar.js

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className='navbar navbar-dark bg-dark'>
+      <div className='container'>
+        <h5 className='navbar-brand'>Corecis</h5>
+        <span className='navbar-text mr-auto'>
+          Todo App React & Eth Smart Contract
+        </span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
