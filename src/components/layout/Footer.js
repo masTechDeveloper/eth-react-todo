14f0e597af8585fb1d125ee59a9e906cd42061e2
masTech Developer <ashir342@gmail.com>
2020-07-31 02:02:37 +0500

    Create Footer.js

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      {/* <footer className=''>
+      <p>Muhammad Ashir Ansari | www.corecis.org</p>
+    </footer> */}
+    </div>
+  );
+}
+
+export default Footer;
