85bf48d861a0d3f81b0f16335aa948445907c992
masTech Developer <ashir342@gmail.com>
2020-07-31 03:05:22 +0500

    Update task.sol

--- SmartContract/task.sol
+++ SmartContract/task.sol
@@ -1,42 +1,34 @@
 pragma solidity ^0.6.0;
 
 contract TodoList {
-  uint public taskCount = 0;
+    uint256 public taskCount = 0;
 
-  struct Task {
-    uint id;
-    string content;
-    bool completed;
-  }
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
 
-  mapping(uint => Task) public tasks;
+    mapping(uint256 => Task) public tasks;
 
-  event TaskCreated(
-    uint id,
-    string content,
-    bool completed
-  );
+    event TaskCreated(uint256 id, string content, bool completed);
 
-  event TaskCompleted(
-    uint id,
-    bool completed
-  );
+    event TaskCompleted(uint256 id, bool completed);
 
-  constructor() public {
-    createTask("Eth Todo List | corecis.com");
-  }
+    constructor() public {
+        createTask("Eth Todo List | www.corecis.org");
+    }
 
-  function createTask(string memory _content) public {
-    taskCount ++;
-    tasks[taskCount] = Task(taskCount, _content, false);
-    emit TaskCreated(taskCount, _content, false);
-  }
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
 
-  function toggleCompleted(uint _id) public {
-    Task memory _task = tasks[_id];
-    _task.completed = !_task.completed;
-    tasks[_id] = _task;
-    emit TaskCompleted(_id, _task.completed);
-  }
-
+    function toggleCompleted(uint256 _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+    }
 }
